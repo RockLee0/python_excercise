16698dd8d80d04c1153c67ccd741014a3182e44d
Rocklee0 <mshakil3847@gmail.com>
2024-08-31 00:38:54 +0600

    hash collusion updated

--- data sturcture/hash collusion handle.py
+++ data sturcture/hash collusion handle.py
@@ -19,6 +19,37 @@ class hashtable:
             h+=ord(char)
         return h%self.max
 
+    def __setitem__(self,key,value):
+        h=self.getHash(key)
+        found=False
+        for idx,element in enumerate(self.arr[h]):
+            if len(element)==2 and element[0]==key:
+                self.arr[h][idx]=(key,value)
+                found=True
+        if not found:
+            self.arr[h].append((key,value))
+    def __getitem__(self, key):
+        h=self.getHash(key)
+        for element in self.arr[h]:
+            if element[0]==key:
+                return element[1]
+    def __delitem__(self, key):
+        h=self.getHash(key)
+        for idx,element in enumerate(self.arr[h]):
+            if element[0]==key:
+                del self.arr[idx]
+
+
+
+
+
+t=hashtable()
+
+
+
+
+
+"""
     def __setitem__(self, key, value):
         h=self.getHash(key)
         self.arr[h]=value
@@ -28,5 +59,4 @@ class hashtable:
     def __delitem__(self,key):
         h=self.getHash(key)
         self.arr[h]=None
-
-t=hashtable()
+"""
