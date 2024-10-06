7a7cbf4442adb64de11178d84c9ff871c37d4801
Rocklee0 <mshakil3847@gmail.com>
2024-10-06 18:08:49 +0600

    binary tree

--- data sturcture/Tree/Binary Tree/binarytree practice.py
+++ data sturcture/Tree/Binary Tree/binarytree practice.py
@@ -1,1 +1,23 @@
-class
+class  BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+    def add_child(self,data):
+        if data==self.data:
+            return
+        if data<self.data:
+            if self.left:
+               self.left.add_child(data)
+            else:
+                self.left=BinarySearchTreeNode(data)
+        if data>self.data:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right=BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elemnets=[]
+        a;ldjf
