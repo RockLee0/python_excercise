de9ecc16ed949195881ecc4694dd3cde541cca4c
Rocklee0 <mshakil3847@gmail.com>
2024-11-10 02:12:50 +0600

    in order traversal implimented

--- data sturcture/Tree/Binary Tree/binarytree practice.py
+++ data sturcture/Tree/Binary Tree/binarytree practice.py
@@ -19,8 +19,21 @@ class  BinarySearchTreeNode:
                 self.right=BinarySearchTreeNode(data)
 
     def in_order_traversal(self):
-        elemnets=[]
+        elements=[]
+
+        if self.left:
+           elements+=self.left.in_order_traversal()
+        elements.append(self.data)
+        if self.right:
+            elements+=self.right.in_order_traversal()
+        return elements
+def build_tree(elements):
+    root=BinarySearchTreeNode(elements[0])
+    for i in elements:
+        BinarySearchTreeNode.add_child(elements[i])
+
+if __name__=='__main__':
+    numbers=[1,8,2,4,9,3,4,7]
 
 
-        d
-        a;ldjf
+
