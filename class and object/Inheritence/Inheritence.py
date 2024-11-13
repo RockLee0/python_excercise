975d7a6980b529f71de4104e207e81684b874f0f
Rocklee0 <mshakil3847@gmail.com>
2024-11-13 15:41:54 +0600

    inheritence implimented by example

--- class and object/Inheritence/Inheritence.py
+++ class and object/Inheritence/Inheritence.py
@@ -1,1 +1,20 @@
-class
+class banor:
+    def general(self):
+        self.tails='Thay have tails'
+        self.mentality='They are rude'
+        print(f'they have this mentality which is {self.mentality}')
+
+class human(banor):
+    def __init__(self):
+        self.legs=5
+        self.dicksize=1
+        self.backbone = 'stand straight'
+        self.family = 'have a family but cheats frequently with others'
+    def skill(self):
+        print(f'he has {self.legs} legs and {self.family} ')
+
+
+
+rony=human()
+print(rony.skill())
+print(rony.general())
