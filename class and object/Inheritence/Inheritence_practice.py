49f1237c9395ce2c323913830afd01289d22cc84
Rocklee0 <mshakil3847@gmail.com>
2024-11-13 16:02:17 +0600

    another example implimented

---
+++ class and object/Inheritence/Inheritence_practice.py
@@ -0,0 +1,25 @@
+class animal:
+    def general_purpose(self):
+        print('we are animal')
+
+class dog:
+    def purpose(self):
+        print('we do ghew ghew all the time')
+
+class human(animal,dog):
+    def __init__(self):
+        self.hand=4
+        self.dick=2
+
+
+    def purpose(self):
+        print(f'we only do masterbedroom with {self.hand} hand and {self.dick} lolopopstar')
+        super().general_purpose()
+
+
+tom=dog()
+print(tom.purpose())
+
+
+murzan=human()
+print(murzan.purpose())
